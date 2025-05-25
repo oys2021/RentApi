5caf3da4f019abe5a5c9a957d7a3c0d65d5fe15c
Yaw Sarfo <76229581+oys2021@users.noreply.github.com>
2025-05-25 21:39:35 +0000

    refactor: change allow host to all

--- Rentz/settings.py
+++ Rentz/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-w0ousdyrqcy#gd%abup2yx(#@j+)-s)rs03hi__jt&ye^v%o_&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.8.127']
+ALLOWED_HOSTS = []
 
 
 # Application definition
