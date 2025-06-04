cd96dfaed956d82e6cc981e3a3791218d046ce08
oys2021 <opokuyawsarfo3@gmail.com>
2025-06-04 19:28:21 +0000

    refactor: add allowed host  route

--- Rentz/settings.py
+++ Rentz/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = 'django-insecure-w0ousdyrqcy#gd%abup2yx(#@j+)-s)rs03hi__jt&ye^v%o_&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'rentz-ecy1.onrender.com',  
+]
 
 
 # Application definition
